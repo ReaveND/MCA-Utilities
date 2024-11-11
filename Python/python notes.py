55af0a2329ffaa67bb67ad82232081d871246c57
ReaveND <reaverrupak@gmail.com>
2024-11-11 13:41:00 +0530

    init

---
+++ Python/python notes.py
@@ -0,0 +1,145 @@
+##print("Hello World");
+##Q. Write a program to print the digit at one's place of a number.
+
+#12345
+
+##n=int(input("Enter the number: "))
+#n=n//10;
+#x=n%10;
+#print(x);
+
+##Write a program to print the ASCII value of a character.
+##Write a program to read a character in upper case and print it in lower case.
+##Write a program to demonstrate implicit and explicit type conversion.
+##Write a program to calculate simple interest and compound interest.
+##Write a program to find x^y.
+##Find the total number of seconds in a day.
+
+##Operators and Expressions in Python.
+#1. Aritmetic Operators.
+#2. Comparison Operators(Relational).
+#3. Assignment Operator.
+#4. Logical Operator.
+#5. Unary Operator.
+#6. Bitwise Operator.
+#7. Membership Operator.
+#8. Identity Operator.
+
+#1. Arithmetic Operator: +, -, *, /, //(floor division), **(exponent), %.
+#2. Comparison Operator: ==, >=, <=,!=,>,<. All these operators return
+#Boolean value, Either True or False.
+
+#3. Assignment Operator and Inplace or Shortcut Operator:
+#b=5
+#a=b
+#print(a)
+
+##a=a+10
+#x=20
+#x+=10
+#print(x)
+
+#+=, -=, *=, /=, //=, %=, **=.
+
+#y=10
+#y**=3
+#print(y)
+
+#name='Rupak'
+#name+=' Sarkar'
+#print(name)
+
+#Ternary Operator:
+#x = a>b?a:b (For C)
+
+#Bitwise Operator: & (AND), | (OR), ^ (X-OR), ~ (Bitwise NOT).
+
+#Shift Operator:
+
+#p=5
+# print(p<<1)
+#It will print the value by doubling the initial value.
+
+#print(p>>1)
+#It will reduce the value by dividing it by 2.
+#q=15
+#print(q>>1) # Output will be 7 (0111).
+
+#Logical Operator: && (and), ||(or), !(not).
+
+#Print the greatest of three numbers using logical operator.
+
+a=10
+b=20
+c=30
+
+if(a>b and a>c):
+    print("a is the greatest.", a)
+elif(b>a and b>c):
+    print("b is the greatest.", b)
+else:
+    print("c is the greatest.", c)
+
+# x++, x-- is not allowed in Python.
+
+#Write a program to check whether a given year is leap year or not.
+
+#Membership Operator:
+#whether the element is present within the sequence or not is
+#checked by the Membership Operator.
+#in, not in.
+#Membership operator will return TRUE or FALSE
+#depending on the result.
+
+#Types of Expression in Python:
+#1. Based on the position of operator the expressions are evaluated.
+#a. Infix Expression. (a+b)
+#b. Prefix Expression. (+ab)
+#c. Postfix Expression. (ab+)
+#Based on Datatype:
+#Constant, Floating, Relational, Logical, Assignment, Bitwise Expressions.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
